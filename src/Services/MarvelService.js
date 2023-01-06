b6f1600e5f45c54c1ef685ba79293bb77b7637fc
SpiderLilyxoxo <spiderlily.wm@gmail.com>
2023-01-06 15:58:26 +0300

    project created

---
+++ src/Services/MarvelService.js
@@ -0,0 +1,26 @@
+
+
+class MarvelService {
+    _apiBase = "https://gateway.marvel.com:443/v1/public/";
+    _apiKey = "apikey=2dbf6b0d41c6b0778a89102291f3348c";
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=260&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService
